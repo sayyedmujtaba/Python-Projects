f9445f99145f68a3d2e9c0907cf619fdef89d86a
Syed Mujtaba <smujtabah14709@gmail.com>
2025-06-04 12:52:39 +0500

    Restaurant management system successfully created

--- Restaurant Management System/Restaurant Management System.py
+++ Restaurant Management System/Restaurant Management System.py
@@ -9,7 +9,7 @@ menu = {
 }
 
 def display_menu():
-    print(colored("Welcome to Python Restaurant", "yellow"))
+    print(colored("Welcome to Python Food Corner", "yellow"))
     for item, price in menu.items():
         print(f"{item.title()}: {price} PKR")
 display_menu()
@@ -17,7 +17,7 @@ display_menu()
 total_bill = 0
 
 while True:
-    order = input(colored("Please enter your order: ", "green")).title() #we can use .capitalize() too but that only capitalize the forst character of a string while .title() capitalize the first character of each word
+    order = input(colored("Please enter your order: ", "green")).title() # We can use .capitalize() too but that only capitalize the forst character of a string while .title() capitalize the first character of each word
     if order in menu:
         total_bill += menu[order]
         print(f"Your bill is {total_bill} PKR")
@@ -26,7 +26,7 @@ while True:
 
     more = input("Do you want to order more? (yes/no): ").strip().lower()
     if more == "yes":
-        continue
+        continue # If the user wants to order more, continue the loop
     elif more == "no":
         print(colored(f"Your total is: {total_bill} PKR", "magenta"))
         break
